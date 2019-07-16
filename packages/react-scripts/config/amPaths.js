b4cd2864e8138cfecfd694b2ab5330f86236bd57
Alex Zywiak <alex.zywiak@aspenmesh.io>
2019-07-16 09:04:33 -0600

    branch root node on env variable

---
+++ packages/react-scripts/config/amPaths.js
@@ -0,0 +1,23 @@
+const SAAS = 'SAAS';
+const SELF_HOSTED = 'SELF_HOSTED';
+const SAAS_DIR = 'saas';
+const SELF_HOSTED_DIR = 'self-hosted';
+
+const getAppIndex = () => {
+  if (process.env.LOCATION === SAAS) {
+    return `src/root/${SAAS_DIR}/index`;
+  }
+  if (process.env.LOCATION === SELF_HOSTED) {
+    return `src/root/${SELF_HOSTED_DIR}/index`;
+  }
+
+  throw new Error(
+    `env variable LOCATION must be set to ${SAAS} or ${SELF_HOSTED}.  Current value is ${
+      process.env.LOCATION
+    }`
+  );
+};
+
+module.exports = {
+  appIndexJs: getAppIndex(),
+};
