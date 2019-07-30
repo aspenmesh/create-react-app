d4edf6fda7b9d6852daa29bebc3d0b800e3bea19
Alex Zywiak <alex.zywiak@aspenmesh.io>
2019-07-30 14:02:54 -0600

    update directory structure

--- packages/react-scripts/config/amPlugins.js
+++ packages/react-scripts/config/amPlugins.js
@@ -2,13 +2,26 @@
 
 const webpack = require('webpack');
 
+const SAAS = 'SAAS';
+const SELF_HOSTED = 'SELF_HOSTED';
+const SAAS_FILE = 'saas';
+const SELF_HOSTED_FILE = 'self-hosted';
+
 module.exports = [
   new webpack.NormalModuleReplacementPlugin(/LOCATION_INTERCEPTORS/, function(
     resource
   ) {
-    resource.request = resource.request.replace(
-      /LOCATION_INTERCEPTORS/,
-      `./${process.env.LOCATION}`
-    );
+    if (process.env.LOCATION === SAAS) {
+      resource.request = resource.request.replace(
+        /LOCATION_INTERCEPTORS/,
+        `./interceptors/${SAAS_FILE}`
+      );
+    }
+    if (process.env.LOCATION === SELF_HOSTED) {
+      resource.request = resource.request.replace(
+        /LOCATION_INTERCEPTORS/,
+        `./interceptors/${SELF_HOSTED_FILE}`
+      );
+    }
   }),
 ];
